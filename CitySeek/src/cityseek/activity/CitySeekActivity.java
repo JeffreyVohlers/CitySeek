991d2df87e5b082094ea86561bc732aa8ad6bcdf
Jeffrey <Jeffrey@152.14.224.238>
2012-06-06 14:25:31 -0400

    Cleaned up code of original push

--- CitySeek/src/cityseek/activity/CitySeekActivity.java
+++ CitySeek/src/cityseek/activity/CitySeekActivity.java
@@ -1,141 +1,62 @@
 package cityseek.activity;
 
-import java.util.List;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Point;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import cityseek.resources.OurLocationOverlay;
 import cityseekers.activity.R;
 
-import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-import com.google.android.maps.MapView.LayoutParams;
-import com.google.android.maps.Overlay;
 
 public class CitySeekActivity extends MapActivity {
 	/** Called when the activity is first created. */
 	MapView mapView;
 	MapController mc;
-	private LocationManager locationManager;
-	private LocationListener locationListener;
+
+	// private LocationManager locationManager;
+	// private LocationListener locationListener;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		initMapView();
+		initMyLocation();
+		// locationManager = (LocationManager)
+		// getSystemService(Context.LOCATION_SERVICE);
+		// locationListener = new GPSLocationListener();
+		// locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+		// 3000, 5, locationListener);
+	}
+
+	private void initMapView() {
 		mapView = (MapView) findViewById(R.id.mapView);
-		LinearLayout zoomLayout = (LinearLayout) findViewById(R.id.zoom);
-		View zoomView = mapView.getZoomControls();
-
-		zoomLayout.addView(zoomView, new LinearLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		mapView.displayZoomControls(true);
-		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-		locationListener = new GPSLocationListener();
-
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				3000, 5, locationListener);
 		mc = mapView.getController();
 		mc.setZoom(12);
+		mapView.setBuiltInZoomControls(true);
+	}
 
+	private void initMyLocation() {
+		final OurLocationOverlay overlay = new OurLocationOverlay(this, mapView);
+		overlay.enableMyLocation();
+		mapView.getOverlays().add(overlay);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				3000, 5, locationListener);
+		// locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+		// 3000, 5, locationListener);
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		locationManager.removeUpdates(locationListener);
+		// locationManager.removeUpdates(locationListener);
 	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
-	}
-
-	class MapOverlay extends Overlay {
-		private GeoPoint pointToDraw;
-
-		public void setPointToDraw(GeoPoint point) {
-			pointToDraw = point;
-		}
-
-		public GeoPoint getPointToDraw() {
-			return pointToDraw;
-		}
-
-		@Override
-		public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
-				long when) {
-			super.draw(canvas, mapView, shadow);
-
-			// convert point to pixels
-			Point screenPts = new Point();
-			mapView.getProjection().toPixels(pointToDraw, screenPts);
-
-			// add marker
-			Bitmap bmp = BitmapFactory.decodeResource(getResources(),
-					R.drawable.urhere);
-			canvas.drawBitmap(Bitmap.createScaledBitmap(bmp, 30, 30, false),
-					screenPts.x, screenPts.y - 15, null);
-			return true;
-		}
-	}
-
-	private class GPSLocationListener implements LocationListener {
-		public void onLocationChanged(Location location) {
-			if (location != null) {
-				GeoPoint point = new GeoPoint(
-						(int) (location.getLatitude() * 1E6),
-						(int) (location.getLongitude() * 1E6));
-				MapOverlay mapOverlay = new MapOverlay();
-				mapOverlay.setPointToDraw(point);
-				List<Overlay> listOfOverlays = mapView.getOverlays();
-				listOfOverlays.clear();
-				listOfOverlays.add(mapOverlay);
-
-				Toast.makeText(
-						getBaseContext(),
-						"Latitude: " + location.getLatitude() + " Longitude: "
-								+ location.getLongitude(), Toast.LENGTH_SHORT)
-						.show();
-
-				mc.animateTo(point);
-				mc.setZoom(16);
-				mapView.invalidate();
-			}
-		}
-
-		public void onProviderDisabled(String arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		public void onProviderEnabled(String arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-			// TODO Auto-generated method stub
-
-		}
 	}
 }
---
+++ CitySeek/src/cityseek/resources/OurLocationOverlay.java
@@ -0,0 +1,58 @@
+package cityseek.resources;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.location.Location;
+import cityseekers.activity.R;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+
+public class OurLocationOverlay extends MyLocationOverlay {
+
+	MapView mapView;
+	Bitmap uRHere;
+	Point currentPoint = new Point();
+
+	boolean centerOnCurrentLocation = true;
+	int height;
+	int width;
+
+	public OurLocationOverlay(Context context, MapView mapView) {
+		super(context, mapView);
+		this.mapView = mapView;
+		uRHere = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+				context.getResources(), R.drawable.urhere), 30, 30, false); // TODO:
+																			// Change
+																			// size
+																			// and
+																			// R
+																			// lookup
+	}
+
+	@Override
+	protected void drawMyLocation(Canvas canvas, MapView mapView,
+			Location lastFix, GeoPoint myLocation, long when) {
+		if (this.height == 0 || this.width == 0) {
+			this.height = mapView.getHeight();
+			this.width = mapView.getWidth();
+		}
+		mapView.getProjection().toPixels(myLocation, currentPoint);
+		canvas.drawBitmap(uRHere, currentPoint.x, currentPoint.y - 40, null); // TODO:
+																				// Change
+																				// proportions
+	}
+
+	@Override
+	public synchronized void onLocationChanged(Location location) {
+		super.onLocationChanged(location);
+		if (mapView != null && centerOnCurrentLocation) {
+			mapView.getController().animateTo(getMyLocation());
+		}
+	}
+
+}
